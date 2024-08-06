7d73049ab20cb36aa1f91a595a9fe97e3253e271
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-06 18:12:57 -0300

    variables

---
+++ js/variables.js
@@ -0,0 +1,23 @@
+// var - let - const
+
+let anio = 2000.56;
+console.log(anio);
+
+let suma = anio + 2;
+console.log(suma);
+
+// parseInt = valor entero
+// parseFloat = valor despues de la coma
+
+const entero = parseInt(anio);
+console.log(entero)
+
+const resta = 2 - 1;
+// alert("la respuesta es " + resta + " es un numero positivo.");
+// alert(`La respues es ${resta} es un numero positivo ${resta}.`);
+
+const numEntero = parseInt(prompt("poneme un numero"));
+
+// alert(`su numero es ${numEntero}`);
+document.write(`<p>su numero es ${numEntero} </p>`)
+document.write("<p>su numero es " + numEntero + "</p>")
